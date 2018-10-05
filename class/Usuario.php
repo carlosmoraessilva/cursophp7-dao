35028a345a1b3ab67e64c65160165e7d80c74b8c
Carlos Moraes da Silva <811355@prodesp-sede.local>
2018-10-05 16:08:37 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -20,6 +20,7 @@
 		public function setDeslogin($value){
 			$this->deslogin = $value;
 		}
+		
 		public function getDessenha(){
 			return $this->dessenha;
 		}
@@ -53,17 +54,55 @@
 		    }
 		}
 
-		public function __toString(){
+		public static function getList(){
 
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+		}
+
+		public static function search($login){
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+				':SEARCH'=>"%".$login."%"
+
+			));
+		}
+
+		public function login($login, $password){
+			$sql = new Sql();
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+				":LOGIN"=>$login,
+				":PASSWORD"=>$password 
+			));
+
+		    if (count($results) > 0){
+
+		    	$row = $results[0];
+
+		    	$this->setIdusuario($row['idusuario']);
+		    	$this->setDeslogin($row['deslogin']);
+		    	$this->setDessenha($row['dessenha']);
+		    	$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} /*else {
+			throw new Exception("Login e/ou senha incorretos.");
+			
+		}*/
+
+	}
+		public function __toString(){
 			return json_encode(array(
 				"idusuario"=>$this->getIdusuario(),
 				"deslogin"=>$this->getDeslogin(),
 				"dessenha"=>$this->getDessenha(),
 				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-
 			));
 		}
 	}
+
 
 
 
--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 
 	require_once("config.php");
 
+	$usuario = new Usuario();
+	$usuario->login("user", '12345');
+	echo $usuario;
+
 	//$sql = new Sql();
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	//echo json_encode($usuarios);
 
-	$root = new Usuario();
+//Carrega um usuario
+/*	$root = new Usuario();
 
 	$root->loadbyId(4);
 
 	echo $root;
+*/
 
+	//Carrega uma lista de usuarios
+
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+	//Carrega uma lista de usuários buscando pelo login
+
+	//$search = Usuario::search("ro");
+
+	//echo json_encode($search);
+
+	//Carrega um usuário usando o login e a senha
+
+	
 
 ?>
